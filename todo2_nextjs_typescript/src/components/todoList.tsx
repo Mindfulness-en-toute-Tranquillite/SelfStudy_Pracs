aa01631de9100d4014a4c661ef195311b60b57cb
Jay <tvghoy@gmail.com>
2023-08-21 19:25:49 +0900

    Todo list - C,U기능 구현 및 D기능 구현중

---
+++ todo2_nextjs_typescript/src/components/todoList.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+const TodoList: React.FunctionComponent = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [newTodo, setNewTodo] = useState('');
+
+    const addTodo = () => {
+        if (newTodo.trim() === '') return;
+
+        const newTodoItem: Todo = {
+            id: Date.now(),
+            text: newTodo,
+            completed: false,
+        }
+        setTodos([...todos, newTodoItem]);
+        setNewTodo('');
+    };
+    const toggleTodo = (id: number) => {
+        const updatedTodos = todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo 
+        );
+        setTodos(updatedTodos);
+    };
+    const deleteTodo = (id: number) => {
+        const updatedTodos = todos.filter((todo) => todo.id !== id)
+        setTodos(updatedTodos);
+    };
+
+    return (
+        <div>
+            <ul>
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        onClick={() => toggleTodo(todo.id)}
+                        style={{
+                            textDecoration: todo.completed ? 'line-through': 'none',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        {todo.text}
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                />
+                <button onClick={addTodo}>Add Todo</button>
+            </div>
+        </div>
+        );
+};
+
+
+
+export default TodoList;
---
+++ todo2_nextjs_typescript/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import TodoList from '../components/todoList';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default Home;
