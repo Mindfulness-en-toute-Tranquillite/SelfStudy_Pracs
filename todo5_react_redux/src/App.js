7b45a9b0fee25c04795c0f5bc73f87529128b5f1
Jay <tvghoy@gmail.com>
2023-08-25 18:34:07 +0900

    counter program prac

--- todo5_react_redux/src/App.js
+++ todo5_react_redux/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const number = useSelector((state) => state.counter.number);
+  console.log(number)
+  const dispatch = useDispatch();
   return (
     <div>
-      
+      <button
+      onClick={() => {
+        dispatch({type : "MINUS_ONE"});
+      }}
+      >-</button>
+      {number}
+      <button
+        onClick={() => {
+          dispatch({type : "PLUS_ONE"});
+        }}
+      >+</button>
     </div>
   );
 }
