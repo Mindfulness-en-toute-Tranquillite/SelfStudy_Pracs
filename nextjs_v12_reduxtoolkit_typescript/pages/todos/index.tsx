177ac307e3deff1a526fd70e0147ea0f05742299
Jay <tvghoy@gmail.com>
2023-09-12 18:27:35 +0900

    [fix]Thunk 생성 및 사용, extraReducer 생성 및  dispatch 에러 해결. 기능구현 에러 해결 필요

--- nextjs_v12_reduxtoolkit_typescript/pages/todos/index.tsx
+++ nextjs_v12_reduxtoolkit_typescript/pages/todos/index.tsx
@@ -1,7 +1,5 @@
 import { RootState } from "@/redux/config/configStore";
-import { addTodos, deleteTodos, toggleTodos } from "@/redux/modules/todosSlice";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { __addTodos, __deleteTodos, __getTodos, __toggleTodos } from "@/redux/modules/todosSlice";
 import React, { SyntheticEvent, useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -18,63 +16,24 @@ export default function Todos() {
   const [inputText, setInputText] = useState<string>('');
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch<any> (__getTodos());
+  }, [dispatch])
+
   const onSubmitHandler = (e: SyntheticEvent) => {
     e.preventDefault();
     if (inputText.trim() === '') 
     return;
-
-    // addTodoAPI(inputText);
-
-    dispatch(addTodos({
-      id: todos.length + 1,
-      title: inputText,
-      complete: false,
-    }))
+    dispatch<any>(__addTodos(inputText))
     setInputText('')
   };
   const onClickDeleteHandler = (id: number) => {
-    dispatch(deleteTodos(id));
+    dispatch<any>(__deleteTodos(id))
   };
   const onClickToggleHandler = (id: number) => {
-    dispatch(toggleTodos(id));
+    dispatch<any>(__toggleTodos(id))
   };
-  const fetchTodos = createAsyncThunk('todos/fetchTodos', async ()=> {
-    try {
-        const response = await axios.get('/api/todos');
-        return response.data.todos;
-    }   catch (error) {
-        throw error;
-    }
-  });
-  useEffect(() => {
-    fetchTodos();
-  }, []);
 
-  // const addTodoAPI = async (newTodo: string) => {
-  //   try {
-  //       await axios.post('/api/todos', { todo: newTodo });
-  //       setInputText('');
-  //       fetchTodos();
-  //   }   catch (error) {
-  //       console.error('There is an error while Todo list is being added', error);
-  //   }
-  // };
-  // const deleteTodoAPI = async (id: number) => {
-  //   try {
-  //     await axios.delete(`/api/todos/${id}`);
-  //     fetchTodos(); // 할 일 목록을 다시 불러옵니다.
-  //   } catch (error) {
-  //     console.error('할 일을 삭제하는 중 오류가 발생했습니다.', error);
-  //   }
-  // };
-  // const toggleTodoAPI = async (id: number) => {
-  //   try {
-  //     await axios.put(`/api/todos/${id}`);
-  //     fetchTodos(); // 할 일 목록을 다시 불러옵니다.
-  //   } catch (error) {
-  //     console.error('할 일을 토글하는 중 오류가 발생했습니다.', error);
-  //   }
-  // }
 
   return(
     <>
@@ -98,7 +57,7 @@ export default function Todos() {
             }}
             onClick={() => onClickToggleHandler(todo.id)}
             >
-              {todo.title}
+              {todo.title}{todo.id}
             <button
               key={todo.id}
               onClick={() => onClickDeleteHandler(todo.id)}
--- nextjs_v12_reduxtoolkit_typescript/redux/modules/todosSlice.ts
+++ nextjs_v12_reduxtoolkit_typescript/redux/modules/todosSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
 interface Todo {
     id: number;
@@ -8,6 +9,46 @@ interface Todo {
 interface TodosState {
     todos: Todo[];
 }
+// 비동기 액션 생성자 정의
+export const __getTodos = createAsyncThunk('todos/getTodos', async () => {
+    try {
+        const response = await axios.get('/api/todos');
+        console.log("response",response)
+        return response.data.todos;
+    } catch (error) {
+        throw error;
+    }
+});
+// __addTodos 액션 생성
+export const __addTodos = createAsyncThunk('todos/addTodos', async (newTodo: string) => {
+    try {
+        const response = await axios.post('/api/todos', { todo: newTodo });
+        return response.data; // 새로 추가된 Todo를 반환
+    } catch (error) {
+        throw error;
+    }
+});
+
+// __deleteTodos 액션 생성
+export const __deleteTodos = createAsyncThunk('todos/deleteTodos', async (id: number) => {
+    try {
+        await axios.delete(`/api/todos/${id}`);
+        return id; // 삭제된 Todo의 ID를 반환
+    } catch (error) {
+        throw error;
+    }
+});
+
+// __toggleTodos 액션 생성
+export const __toggleTodos = createAsyncThunk('todos/toggleTodos', async (id: number) => {
+    try {
+        await axios.put(`/api/todos/${id}`);
+        return id; // 토글된 Todo의 ID를 반환
+    } catch (error) {
+        throw error;
+    }
+});
+
 
 const todosSlice = createSlice({
     name: "todos",
@@ -15,19 +56,40 @@ const todosSlice = createSlice({
         todos: [],
     } as TodosState,
     reducers: {
-        addTodos: (state, action: PayloadAction<Todo>) => {
-            state.todos.push(action.payload);
-        },
-        deleteTodos: (state, action: PayloadAction<number>) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-        },
-        toggleTodos: (state, action: PayloadAction<number>) => {
-            const todo = state.todos.find((todo) => todo.id === action.payload)
-            if (todo)
-            todo.complete = !todo.complete
-        }
+    },
+        // addTodos: (state, action: PayloadAction<Todo>) => {
+        //     state.todos.push(action.payload);
+        // },
+        // deleteTodos: (state, action: PayloadAction<number>) => {
+        //     state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        // },
+        // toggleTodos: (state, action: PayloadAction<number>) => {
+        //     const todo = state.todos.find((todo) => todo.id === action.payload)
+        //     if (todo)
+        //     todo.complete = !todo.complete
+        // }
+        extraReducers: (builder) => {
+            // createAsyncThunk로 생성한 액션을 처리
+            builder.addCase(__getTodos.fulfilled, (state, action) => {
+                state.todos = action.payload;
+            })
+            .addCase(__addTodos.fulfilled, (state, action) => {
+                // 추가한 항목을 state에 추가
+                state.todos.push(action.payload);
+            })
+            .addCase(__deleteTodos.fulfilled, (state, action) => {
+                // 삭제한 항목을 state에서 제거
+                state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+            })
+            .addCase(__toggleTodos.fulfilled, (state, action) => {
+                // 토글한 항목의 complete 상태를 변경
+                const todo = state.todos.find((t) => t.id === action.payload);
+                if (todo) {
+                    todo.complete = !todo.complete;
+                }
+            });
     },
 })
 
-export const { addTodos, deleteTodos, toggleTodos } =  todosSlice.actions;
+// export const { addTodos, deleteTodos, toggleTodos } =  todosSlice.actions;
 export default todosSlice.reducer;
